6f4e3ce4367ef6026a70a1c0dd8ca1ce815b0e20
Joselyn Chavez <joselynchavezf@gmail.com>
2020-08-05 01:55:58 -0500

    add documentation

--- R/suma.R
+++ R/suma.R
@@ -1,3 +1,11 @@
+#' Suma dos numeros enteros o decimales
+#'
+#' @param x Un numero entero o decimal
+#' @param y Un numero entero o decimal
+#'
+#' @return Un numero entero o decimal
+#' @export
+#'
 suma <- function(x,y) {
     stopifnot("'x' y 'y' deben ser de tipo numerico " =
                   is.numeric(x) && is.numeric(y),
