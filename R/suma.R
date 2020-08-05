d8119bb03a4c1613118090fbd15bed53cdf06601
Joselyn Chavez <joselynchavezf@gmail.com>
2020-08-05 01:55:20 -0500

    add stopifnot

--- R/suma.R
+++ R/suma.R
@@ -1,4 +1,7 @@
 suma <- function(x,y) {
+    stopifnot("'x' y 'y' deben ser de tipo numerico " =
+                  is.numeric(x) && is.numeric(y),
+    )
     z <- sum(x,y)
     print(paste("La suma de",x,"mas",y,"es:",z))
 }
