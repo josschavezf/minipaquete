a2c7ceca386170bc786c90926a9d96dc59f78fd5
Joselyn Chavez <joselynchavezf@gmail.com>
2020-08-05 01:56:37 -0500

    add examples

--- R/suma.R
+++ R/suma.R
@@ -5,7 +5,9 @@
 #'
 #' @return Un numero entero o decimal
 #' @export
-#'
+#' @examples
+#' suma(3, 4)
+#' suma(3.14, 1.3333)
 suma <- function(x,y) {
     stopifnot("'x' y 'y' deben ser de tipo numerico " =
                   is.numeric(x) && is.numeric(y),
